f58cbc333dd75ba5601f0df07e84f84c0ce136d3
Matth_eu <matthew@matth.eu>
2016-11-11 11:16:41 +0000

    initial commit

---
+++ cmb-date-timezone-field.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     CMB Date Timezone Field
+ * Plugin URI:      https://github.com/mattheu/cmb-date-timezone-field
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          Matthew Haines-Young
+ * Author URI:      http://matth.eu
+ * Text Domain:     cmb-date-timezone-field
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Cmb_Date_Timezone_Field
+ */
